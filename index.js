c48742de3ec6e3303d036adf0213c85f7208e820
gangwar22 <147224060+gangwar22@users.noreply.github.com>
2024-06-29 16:58:45 +0530

    Add html css and index.js code

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ const getCityCoordinates = () => {
 }
 
 
+
 // Function to get weather details based on coordinates
 const getWeatherDetails = (lat, lon) => {
     const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`;
