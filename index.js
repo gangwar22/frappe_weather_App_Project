91569015191aead02513e901a4a701bcce0be912
Priyanka-rathore21 <priyu2119@gmail.com>
2024-06-29 15:04:06 +0530

    add getWeatherDetailsAPI

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+// Function to get weather details based on coordinates
+const getWeatherDetails = (lat, lon) => {
+    const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`;
 
+    fetch(WEATHER_API_URL)
+        .then(response => response.json())
+        .then(data => {
+            displayWeatherDetails(data);
+            saveLastSearch(data); // Save the data to local storage
+            console.log("data fetch", data);
+        })
+        .catch(() => {
+            alert("An error occurred while fetching the weather details!");
+        });
+}
 
